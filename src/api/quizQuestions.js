ef58aae733d2a0f7ec72d32afd553af975959185
Zayooo00 <konrad6263@o2.pl>
2022-11-03 16:03:44 +0100

    Bugfix

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -1,109 +1,109 @@
 var quizQuestions = [
-  {
-      question: "Które zdanie najlepiej cie opisuje?",
-      answers: [
-          {
-              type: "Flegmatykiem",
-              content: "Jestem obserwatorem"
-          },
-          {
-              type: "Cholerykiem",
-              content: "Jestem człowiekiem czynu"
-          },
-          {
-              type: "Sangwinikiem",
-              content: "Jestem emocjonalny"
-          },
-          {
-            type: "Melancholikiem",
-            content: "Jestem myślicielem"
-        },
-      ]
-  },
-  {
-      question: "Jak najlepiej ci się pracuje?",
-      answers: [
-        {
-            type: "Flegmatykiem",
-            content: "W spokoju i bez nagłych sytuacji"
-        },
-        {
-            type: "Cholerykiem",
-            content: "Z pozycji szefa/szefowej"
-        },
-        {
-            type: "Sangwinikiem",
-            content: "W większym gronie osób"
-        },
-        {
-          type: "Melancholikiem",
-          content: "W samotności"
-      },
-      ]
-  },
-  {
-      question: "W dzieciństwie często...",
-      answers: [
-        {
-            type: "Flegmatykiem",
-            content: "Tak"
-        },
-        {
-            type: "Cholerykiem",
-            content: "kłóciłeś/aś się"
-        },
-        {
-            type: "Sangwinikiem",
-            content: "płakałeś/aś"
-        },
-        {
-          type: "Melancholikiem",
-          content: "siedziałeś/aś sam"
-      },
-      ]
-  },
-  {
-      question: "Jak reagujesz na niepowodzenia?",
-      answers: [
-        {
-            type: "Flegmatykiem",
-            content: "Szukam innego sposobu"
-        },
-        {
-            type: "Cholerykiem",
-            content: "Denerwuje się"
-        },
-        {
-            type: "Sangwinikiem",
-            content: "Próbuję ponownie"
-        },
-        {
-          type: "Melancholikiem",
-          content: "Załamuje się"
-      },
-      ]
-  },
-  {
-      question: "Który kolor lubisz najbardziej?",
-      answers: [
-        {
-            type: "Flegmatykiem",
-            content: "Zielony"
-        },
-        {
-            type: "Cholerykiem",
-            content: "Zółty"
-        },
-        {
-            type: "Sangwinikiem",
-            content: "Czerwony"
-        },
-        {
-          type: "Melancholikiem",
-          content: "Niebieski"
-      },
-      ]
-  }
+    {
+        question: "Które zdanie najlepiej cie opisuje?",
+        answers: [
+            {
+                type: "Jesteś Flegmatykiem",
+                content: "Jestem obserwatorem"
+            },
+            {
+                type: "Jesteś Cholerykiem",
+                content: "Jestem człowiekiem czynu"
+            },
+            {
+                type: "Jesteś Sangwinikiem",
+                content: "Jestem emocjonalny"
+            },
+            {
+                type: "Jesteś Melancholikiem",
+                content: "Jestem myślicielem"
+            },
+        ]
+    },
+    {
+        question: "Jak najlepiej ci się pracuje?",
+        answers: [
+            {
+                type: "Jesteś Flegmatykiem",
+                content: "W spokoju i bez nagłych sytuacji"
+            },
+            {
+                type: "Jesteś Cholerykiem",
+                content: "Z pozycji szefa/szefowej"
+            },
+            {
+                type: "Jesteś Sangwinikiem",
+                content: "W większym gronie osób"
+            },
+            {
+                type: "Jesteś Melancholikiem",
+                content: "W samotności"
+            },
+        ]
+    },
+    {
+        question: "W dzieciństwie często...",
+        answers: [
+            {
+                type: "Jesteś Flegmatykiem",
+                content: "poznawałeś/aś inne osoby"
+            },
+            {
+                type: "Jesteś Cholerykiem",
+                content: "kłóciłeś/aś się"
+            },
+            {
+                type: "Jesteś Sangwinikiem",
+                content: "płakałeś/aś"
+            },
+            {
+                type: "Jesteś Melancholikiem",
+                content: "siedziałeś/aś sam"
+            },
+        ]
+    },
+    {
+        question: "Jak reagujesz na niepowodzenia?",
+        answers: [
+            {
+                type: "Jesteś Flegmatykiem",
+                content: "Szukam innego sposobu"
+            },
+            {
+                type: "Jesteś Cholerykiem",
+                content: "Denerwuje się"
+            },
+            {
+                type: "Jesteś Sangwinikiem",
+                content: "Próbuję ponownie"
+            },
+            {
+                type: "Jesteś Melancholikiem",
+                content: "Załamuje się"
+            },
+        ]
+    },
+    {
+        question: "Który kolor lubisz najbardziej?",
+        answers: [
+            {
+                type: "Jesteś Flegmatykiem",
+                content: "Zielony"
+            },
+            {
+                type: "Jesteś Cholerykiem",
+                content: "Zółty"
+            },
+            {
+                type: "Jesteś Sangwinikiem",
+                content: "Czerwony"
+            },
+            {
+                type: "Jesteś Melancholikiem",
+                content: "Niebieski"
+            },
+        ]
+    }
 ];
 
 export default quizQuestions;
