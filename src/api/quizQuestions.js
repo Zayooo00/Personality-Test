53ad4d5066eb08c7620337674776f889849b45fe
Zayooo00 <konrad6263@o2.pl>
2022-11-03 15:25:37 +0100

    first commit

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,109 @@
+var quizQuestions = [
+  {
+      question: "Które zdanie najlepiej cie opisuje?",
+      answers: [
+          {
+              type: "Flegmatykiem",
+              content: "Jestem obserwatorem"
+          },
+          {
+              type: "Cholerykiem",
+              content: "Jestem człowiekiem czynu"
+          },
+          {
+              type: "Sangwinikiem",
+              content: "Jestem emocjonalny"
+          },
+          {
+            type: "Melancholikiem",
+            content: "Jestem myślicielem"
+        },
+      ]
+  },
+  {
+      question: "Jak najlepiej ci się pracuje?",
+      answers: [
+        {
+            type: "Flegmatykiem",
+            content: "W spokoju i bez nagłych sytuacji"
+        },
+        {
+            type: "Cholerykiem",
+            content: "Z pozycji szefa/szefowej"
+        },
+        {
+            type: "Sangwinikiem",
+            content: "W większym gronie osób"
+        },
+        {
+          type: "Melancholikiem",
+          content: "W samotności"
+      },
+      ]
+  },
+  {
+      question: "W dzieciństwie często...",
+      answers: [
+        {
+            type: "Flegmatykiem",
+            content: "Tak"
+        },
+        {
+            type: "Cholerykiem",
+            content: "kłóciłeś/aś się"
+        },
+        {
+            type: "Sangwinikiem",
+            content: "płakałeś/aś"
+        },
+        {
+          type: "Melancholikiem",
+          content: "siedziałeś/aś sam"
+      },
+      ]
+  },
+  {
+      question: "Jak reagujesz na niepowodzenia?",
+      answers: [
+        {
+            type: "Flegmatykiem",
+            content: "Szukam innego sposobu"
+        },
+        {
+            type: "Cholerykiem",
+            content: "Denerwuje się"
+        },
+        {
+            type: "Sangwinikiem",
+            content: "Próbuję ponownie"
+        },
+        {
+          type: "Melancholikiem",
+          content: "Załamuje się"
+      },
+      ]
+  },
+  {
+      question: "Który kolor lubisz najbardziej?",
+      answers: [
+        {
+            type: "Flegmatykiem",
+            content: "Zielony"
+        },
+        {
+            type: "Cholerykiem",
+            content: "Zółty"
+        },
+        {
+            type: "Sangwinikiem",
+            content: "Czerwony"
+        },
+        {
+          type: "Melancholikiem",
+          content: "Niebieski"
+      },
+      ]
+  }
+];
+
+export default quizQuestions;
