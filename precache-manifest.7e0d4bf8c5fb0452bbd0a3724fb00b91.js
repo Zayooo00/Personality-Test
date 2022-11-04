f41ccd28fdc429e75a2bf50081b2c2293de4d992
Zayooo00 <konrad6263@o2.pl>
2022-11-04 16:34:28 +0100

    Updates

---
+++ precache-manifest.7e0d4bf8c5fb0452bbd0a3724fb00b91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6af32726d8b326d6bbde",
+    "url": "/Personality-Test/static/css/main.94ddaf04.chunk.css"
+  },
+  {
+    "revision": "6af32726d8b326d6bbde",
+    "url": "/Personality-Test/static/js/main.6af32726.chunk.js"
+  },
+  {
+    "revision": "6186735684b095a0a50a",
+    "url": "/Personality-Test/static/js/1.61867356.chunk.js"
+  },
+  {
+    "revision": "22cc4d78eb2aa558cad5",
+    "url": "/Personality-Test/static/js/runtime~main.22cc4d78.js"
+  },
+  {
+    "revision": "4cffa739f751f90686ec3ac6e9b56e0d",
+    "url": "/Personality-Test/static/media/icon-check.4cffa739.svg"
+  },
+  {
+    "revision": "3f49f408ef034d42d9d047c03f46c9e1",
+    "url": "/Personality-Test/index.html"
+  }
+];
